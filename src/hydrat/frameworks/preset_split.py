04c9280c05b467f90ffbab12cea97ca960c0ce04
Marco Lui <saffsd@gmail.com>
2010-08-27 11:27:43 +1000

    Developed preset_split to work with both train/test splits as well as cross-validation type folds.

--- src/hydrat/frameworks/preset_split.py
+++ src/hydrat/frameworks/preset_split.py
@@ -32,13 +32,29 @@ class PresetSplitFramework(Framework):
     self.split_name = None
 
   def set_split(self, split):
+    # TODO: Work out the type of preset split!!!
     self.notify("Setting split to '%s'" % split)
-    split_raw = self.dataset.split(split)
-    all_ids = self.dataset.instance_ids
-    train_ids = membership_vector(all_ids, split_raw['train'])
-    test_ids = membership_vector(all_ids, split_raw['test'])
-    self.split = numpy.dstack((train_ids, test_ids)).swapaxes(0,1)
     self.split_name = split
+    split_raw = self.dataset.split(split)
+    if 'train' in split_raw and 'test' in split_raw:
+      # Train/test type split.
+      all_ids = self.dataset.instance_ids
+      train_ids = membership_vector(all_ids, split_raw['train'])
+      test_ids = membership_vector(all_ids, split_raw['test'])
+      self.split = numpy.dstack((train_ids, test_ids)).swapaxes(0,1)
+    elif any(key.startswith('fold') for key in split_raw):
+      # Cross-validation folds
+      all_ids = self.dataset.instance_ids
+      folds_present = sorted(key for key in split_raw if key.startswith('fold'))
+      partitions = []
+      for fold in folds_present:
+        test_ids = membership_vector(all_ids, split_raw[fold])
+        train_docids = sum((split_raw[f] for f in folds_present if f is not fold), [])
+        train_ids = membership_vector(all_ids, train_docids)
+        partitions.append( numpy.dstack((train_ids, test_ids)).swapaxes(0,1) )
+      self.split = numpy.hstack(partitions)
+    else:
+      raise ValueError, "Unknown type of split"
     self.configure()
 
   def is_configurable(self):
