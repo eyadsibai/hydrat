a67fc6804485436d1cdd9e624418f55808491490
Marco Lui <saffsd@gmail.com>
2010-09-22 08:55:40 +1000

    Fixed a bug in extractors that did not pass the correct order of N.

--- src/hydrat/common/extractors.py
+++ src/hydrat/common/extractors.py
@@ -7,12 +7,17 @@ def ngram_dist(n, ts):
   if isinstance(ts, list):
     # Need to handle lists specially as a slice of a list is a list,
     # which is unhashable and thus incompatible with counter.
-    return Counter(NGram(1)(tuple(ts)))
+    return Counter(NGram(n)(tuple(ts)))
   else:
-    return Counter(NGram(1)(ts))
+    return Counter(NGram(n)(ts))
+
+def unordered_ngram_dist(n, ts):
+  return Counter(NGram(n)(tuple(sorted(ts))))
 
 def unigram(ts):   return ngram_dist(1, ts) 
 def bigram(ts):    return ngram_dist(2, ts) 
 def trigram(ts):   return ngram_dist(3, ts)
 def quadgram(ts):  return ngram_dist(4, ts)
 def pentagram(ts): return ngram_dist(5, ts)
+
+def unordered_bigram(ts): return unordered_ngram_dist(2, ts)
