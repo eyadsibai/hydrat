f4033302845d4535d00294d882253a53cc606443
Marco Lui <saffsd@gmail.com>
2010-09-28 15:35:34 +1000

    refactored common code for frameworks

---
+++ src/hydrat/frameworks/common.py
@@ -0,0 +1,19 @@
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+def init_workdir(path, newdirs=["models","tasks","results","output"]):
+  """ Initialize the working directory, where various intermediate files will be stored.
+  This is not to be considered a scratch folder, since the files stored here can be re-used.
+  @param path The path to initialize
+  """
+  if os.path.exists(path):
+    logger.warning('%s already exists', path)
+  else:
+    os.makedirs(path)
+    for dir in newdirs:
+      os.mkdir(os.path.join(path,dir))
+
+
+  
