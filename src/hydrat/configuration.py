649f0d6c374b44b4925c565aa761a92e6651ad01
Marco Lui <saffsd@gmail.com>
2010-10-12 15:45:40 +1100

    changed the default result path

--- src/hydrat/configuration.py
+++ src/hydrat/configuration.py
@@ -124,7 +124,7 @@ def default_configuration():
   default_config.set('paths', 'scratch', '/tmp')
   default_config.set('paths', 'models', '%(work)s/models')
   default_config.set('paths', 'tasks', '%(work)s/tasks')
-  default_config.set('paths', 'results', '%(work)s/results')
+  default_config.set('paths', 'results', './results')
   default_config.set('paths', 'output', '%(work)s/output')
   default_config.set('paths', 'corpora', '~/data')
 
