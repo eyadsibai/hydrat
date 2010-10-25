1a1933d9b3f680dca8bf199c8e62fd030bcbd106
Marco Lui <saffsd@gmail.com>
2010-10-25 15:25:49 +1100

    updated browser to use new-style summary functions

--- src/hydrat/browser/browser_config.py
+++ src/hydrat/browser/browser_config.py
@@ -1,8 +1,22 @@
-from hydrat.display.summary_fns import sf_basic
-summary_fn = sf_basic
+from hydrat.summary import classification_summary
+summary_fn = classification_summary()
 
 from hydrat.result.interpreter import SingleHighestValue
 interpreter = SingleHighestValue()
 
-from hydrat.display.tsr import default_relevant 
-relevant = default_relevant
+relevant  = \
+            [ ( {'label':"Dataset", 'searchable':True}       , "dataset"       )
+            , ( {'label':"Class Space",'searchable':True}     , "class_space"     )
+            , ( {'label':"Feature Desc",'searchable':True}   , "feature_desc"     )
+            , ( {'label':"Learner",'searchable':True}    , "learner"    )
+            , ( {'label':"Params",'searchable':True}    , "learner_params"    )
+            , ( "Macro-F"       , "macro_fscore"        )
+            , ( "Macro-P"     , "macro_precision"     )
+            , ( "Macro-R"        , "macro_recall"        )
+            , ( "Micro-F"       , "micro_fscore"        )
+            , ( "Micro-P"     , "micro_precision"     )
+            , ( "Micro-R"        , "micro_recall"        )
+            , ( {'sorter':'digit', 'label':"Learn Time"}    , "avg_learn"     )
+            , ( {'sorter':'digit', 'label':"Classify Time"} , "avg_classify"  )
+            , ( {'sorter': None, 'label':"Details"}      , "link"          )
+            ]
