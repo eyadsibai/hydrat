9deb3c50b5e8b4abeb694bfee3ee5a004c7f03cc
Marco Lui <saffsd@gmail.com>
2010-10-28 10:13:34 +1100

    added logging of timing metadata to task transformation

--- src/hydrat/task/transform.py
+++ src/hydrat/task/transform.py
@@ -1,18 +1,31 @@
 from hydrat.task.taskset import TaskSet
 from hydrat.task.task import Task
+import time
 
 def transform_task(task, transformer):
+  start = time.time()
   transformer.learn(task.train_vectors, task.train_classes)
+  learn_time = time.time() - start
+
   t = Task()
   affected = ['train_vectors', 'test_vectors']
+  start = time.time()
   for slot in Task.__slots__:
     if slot in affected:
       setattr(t, slot, transformer.apply(getattr(task, slot)))
     else:
       setattr(t, slot, getattr(task, slot))
+  apply_time = time.time() - start
+
   # Separately update metadata
   t.metadata      = dict(task.metadata)
   t.metadata['feature_desc']+=(transformer.__name__,)
+  if 'transform_learn_time' not in t.metadata:
+    t.metadata['transform_learn_time'] = {}
+  if 'transform_apply_time' not in t.metadata:
+    t.metadata['transform_apply_time'] = {}
+  t.metadata['transform_learn_time'][transformer.__name__] = learn_time
+  t.metadata['transform_apply_time'][transformer.__name__] = apply_time
   return t
   
 def transform_taskset(taskset, transformer):
