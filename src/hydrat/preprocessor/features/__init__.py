97787db5cf6c4cee459a86c29ad1eb6a2afdced8
Marco Lui <saffsd@gmail.com>
2010-10-30 15:07:51 +1100

    gave FeatureMap and ClassMap default empty metadata

--- src/hydrat/preprocessor/features/__init__.py
+++ src/hydrat/preprocessor/features/__init__.py
@@ -1,6 +1,6 @@
 from copy import deepcopy
 class FeatureMap(object):
-  def __init__(self, raw, metadata):
+  def __init__(self, raw, metadata = {}):
     self.raw = raw
     self.metadata = deepcopy(metadata)
     if 'feature_desc' not in metadata:
