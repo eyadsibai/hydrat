6afe0080b8b0cd783641f63acbcd11dc70002833
Marco Lui <saffsd@gmail.com>
2010-11-05 17:06:24 +1100

    made markov functions aware of the additional arguments exposed by add_args on learners

--- src/hydrat/classifier/sequence/markov.py
+++ src/hydrat/classifier/sequence/markov.py
@@ -3,6 +3,7 @@ import numpy
 from hydrat.classifier.abstract import Learner, Classifier
 import hydrat.common.pb as pb
 from hydrat.common.sequence import topological_sort
+from hydrat.transformer.abstract import filter_kwargs
 
 class MarkovLearner(Learner):
   def __init__(self, learner, markov_functions):
@@ -13,49 +14,53 @@ class MarkovLearner(Learner):
 
   def _params(self):
     params = dict(self.learner.params)
-    params['markov_functions'] =  [ f.__name__ for f in self.markov_functions ]
+    params['markov_functions'] =  tuple( f.__name__ for f in self.markov_functions )
     return params
 
-  def _learn(self, feature_map, class_map, sequence):
+  def _learn(self, feature_map, class_map, **kwargs):
     features = []
     for i in xrange(feature_map.shape[0]):
       vectors = []
       for f in self.markov_functions:
-        markov_vector = f(feature_map, class_map, sequence, i)
+        supported_kwargs = filter_kwargs(f, 3, kwargs)
+        markov_vector = f(feature_map, class_map, i, **supported_kwargs)
         vectors.append(markov_vector)
       features.append(scipy.sparse.hstack(vectors))
     fm = scipy.sparse.vstack(features).tocsr()
 
+    # TODO: kwarg passthrough?
     classif = self.learner(fm, class_map)
     return MarkovClassifier\
              ( classif
              , self.markov_functions
              , class_map.shape[1]
+             , fm.dtype
              )
       
 
 class MarkovClassifier(Classifier):
-  def __init__(self, classif, markov_functions, num_class):
+  def __init__(self, classif, markov_functions, num_class, dtype):
     self.__name__ = classif.__name__
     Classifier.__init__(self)
     self.classif = classif
     self.markov_functions = markov_functions
     self.num_class = num_class
+    self.dtype = dtype
 
-  def _classify(self, feature_map, sequence):
-    class_map = None
+  def _classify(self, feature_map, **kwargs):
+    class_map = numpy.empty((feature_map.shape[0], self.num_class), dtype=self.dtype)
     pb.ENABLED = False
     # Traverse in topological sort order to ensure that we don't process any instance
     # before processing its parents. 
+    sequence = kwargs['sequence']
     for i in topological_sort(sequence):
       # TODO: Hstack the markov functions
       vectors = []
       for f in self.markov_functions:
-        markov_vector = f(feature_map, class_map, sequence, i)
+        supported_kwargs = filter_kwargs(f, 3, kwargs)
+        markov_vector = f(feature_map, class_map, i, **supported_kwargs)
         vectors.append(markov_vector)
       classif_vector = self.classif(scipy.sparse.hstack(vectors).tocsr())
-      if class_map is None:
-        class_map = numpy.empty((feature_map.shape[0], self.num_class), dtype=markov_vector.dtype)
       class_map[i] = classif_vector
     pb.ENABLED = True
     return class_map
