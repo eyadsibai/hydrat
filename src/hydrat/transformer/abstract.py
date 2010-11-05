cc7a9d5d235f90dd080020eb4bf97adbdb5e396d
Marco Lui <saffsd@gmail.com>
2010-11-05 17:04:44 +1100

    introduced a mechanism to specify additional (unrecorded) args to transforms

--- src/hydrat/transformer/abstract.py
+++ src/hydrat/transformer/abstract.py
@@ -1,4 +1,19 @@
 import logging
+import inspect
+
+
+def filter_kwargs(fn, skip, kwargs):
+  argspec = inspect.getargspec(fn)
+  if argspec.keywords is not None:
+    supported_kwargs = dict(kwargs)
+  else:
+    supported_kwargs = dict()
+    for key in argspec.args[skip:]:
+      try:
+        supported_kwargs[key] = kwargs[key]
+      except KeyError:
+        raise ValueError, "arg %s not available!" % (key)
+  return supported_kwargs
 
 class Transformer(object):
   def __init__(self):
@@ -10,13 +25,22 @@ class Transformer(object):
   def __str__(self):
     return '<Transformer %s>' % self.__name__
 
-  def learn(feature_map, class_map):
-    raise NotImplemented
+  def learn(self, feature_map, class_map):
+    raise NotImplementedError
 
-  def apply(feature_map):
-    raise NotImplemented
+  def apply(self, feature_map):
+    raise NotImplementedError
+
+  def _learn(self, feature_map, class_map, add_args):
+    supported_kwargs = filter_kwargs(self.learn, 3, add_args)
+    retval = self.learn(feature_map, class_map, **supported_kwargs)
+    if retval is not None:
+      self.logger.critical('learn returned %s', str(retval))
+
+  def _apply(self, feature_map, add_args):
+    supported_kwargs = filter_kwargs(self.apply, 2, add_args)
+    return self.apply(feature_map, **supported_kwargs)
 
 class LearnlessTransformer(Transformer):
-  def learn(self, feature_map, class_map):
-    pass
+  def learn(self, feature_map, class_map): pass
 
