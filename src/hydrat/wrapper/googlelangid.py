0b79ac1080bb2b819b152ac5408e3ec8a282b398
Marco Lui <saffsd@gmail.com>
2010-11-16 10:35:06 +1100

    added an api key option to the langid interface

--- src/hydrat/wrapper/googlelangid.py
+++ src/hydrat/wrapper/googlelangid.py
@@ -13,23 +13,27 @@ logger = logging.getLogger(__name__)
 class GoogleLangid(object):
   base_url='http://ajax.googleapis.com/ajax/services/language/detect?'
 
-  def __init__( self, sleep=5, simulate=False, retry = 60 ):
+  def __init__( self, sleep=5, simulate=False, retry = 60, apikey=None ):
     self.sleep = sleep # Time between requests
     self.simulate = simulate
     self.retry = retry
+    self.apikey = apikey
 
   def classify(self, text):
     if isinstance(text, unicode): text = text.encode('utf-8')
     if self.simulate:
       response = {'responseData':{'language':'en'}}
     else:
-      data = urllib.urlencode({'v':1.0,'ie': 'UTF8', 'q': text})
+      query = {'v':1.0,'ie': 'UTF8', 'q': text}
+      if self.apikey is not None:
+        query['key'] = self.apikey
+      data = urllib.urlencode(query)
       search_results = urllib.urlopen(self.base_url+data)
       response = json.loads(search_results.read())
     retry = self.retry
     while response['responseData'] is None:
       logger.warning(response)
-      logger.warning("Got a None response, retrying in %d seconds", self.retry)
+      logger.warning("Got a None response, retrying in %d seconds", retry)
       time.sleep(retry)
       retry *= 2
       search_results = urllib.urlopen(self.base_url+data)
