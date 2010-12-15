b337739b111cee9e5deb1112de7a3cd991787a93
Marco Lui <saffsd@gmail.com>
2010-12-15 11:47:05 +1100

    split textcat into separate functions for train and apply

--- src/hydrat/langid/textcat.py
+++ src/hydrat/langid/textcat.py
@@ -1,4 +1,5 @@
 import time
+import logging
 from hydrat.result.tasksetresult import TaskSetResult
 from hydrat.result.result import Result
 from hydrat.store import Store
@@ -9,47 +10,64 @@ from hydrat import config
 from hydrat.configuration import Configurable, EXE
 from hydrat.task.sampler import membership_vector
 
+logger = logging.getLogger(__name__)
+
 class TextCatConfig(Configurable):
   requires =\
     { ('tools', 'textcat')    : EXE('text_cat')
     }
 
-def do_textcat(train_ds, test_ds, tokenstream, class_space, classlabels, instance_labels):
-  md = dict(\
-    class_space  = class_space,
-    dataset      = train_ds.__name__,
-    eval_dataset = test_ds.__name__,
-    instance_space = train_ds.instance_space,
-    eval_space   = test_ds.instance_space,
-    learner      = 'textcat',
-    learner_params = dict(tokenstream=tokenstream),
-    )
-
+def train_textcat(ds, tokenstream, class_space):
+  logger.info('train textcat')
   cat = TextCat(config.getpath('tools','textcat'), config.getpath('paths','scratch'))
-  train_ts = train_ds.tokenstream(tokenstream)
-  train_cm = train_ds.classmap(class_space)
-  pairs = [ (train_ts[i], train_cm[i][0]) for i in train_ds.instance_ids ]
+  ts = ds.tokenstream(tokenstream)
+  cm = ds.classmap(class_space)
+  pairs = [ (ts[i], cm[i][0]) for i in ds.instance_ids ]
 
   start = time.time()
   cat.train(pairs)
   train_time = time.time() - start
 
-  test_ts = test_ds.tokenstream(tokenstream)
-  class_map = {}
+  cat.metadata = dict(
+    dataset = ds.__name__, 
+    instance_space = ds.instance_space, 
+    tokenstream=tokenstream, 
+    class_space=class_space,
+    train_time=train_time,
+    )
+  return cat
+
+def do_textcat(cat, ds, classlabels, instance_labels):
+  logger.info('do textcat')
+  class_space = cat.metadata['class_space']
+  tokenstream = cat.metadata['tokenstream']
+  md = dict(\
+    class_space  = class_space,
+    dataset      = cat.metadata['dataset'],
+    eval_dataset = ds.__name__,
+    instance_space = cat.metadata['instance_space'],
+    eval_space   = ds.instance_space,
+    learner      = 'textcat',
+    learner_params = dict(tokenstream=tokenstream),
+    )
+
+  ts = ds.tokenstream(tokenstream)
   start = time.time()
-  for id in test_ts:
-    klass = cat.classify(test_ts[id])
-    class_map[id] = [ klass ]
+  ids, texts = zip(*ts.items())
+  klass = cat.batch_classify(texts)
+  class_map = {}
+  for id, cl in zip(ids, klass):
+    class_map[id] = [ cl ]
   test_time = time.time() - start
   
 
-  cl = map2matrix( class_map, test_ds.instance_ids, classlabels )
-  gs = map2matrix( test_ds.classmap(class_space), test_ds.instance_ids, classlabels )
+  cl = map2matrix( class_map, ds.instance_ids, classlabels )
+  gs = map2matrix( ds.classmap(class_space), ds.instance_ids, classlabels )
 
   result_md = dict(md)
-  result_md['learn_time'] = train_time
+  result_md['learn_time'] = cat.metadata['train_time']
   result_md['classify_time'] = test_time
-  instance_indices = membership_vector(instance_labels, test_ds.instance_ids)
+  instance_indices = membership_vector(instance_labels, ds.instance_ids)
   result = Result(gs, cl, instance_indices, result_md )
   tsr = TaskSetResult( [result], md )
   return tsr
