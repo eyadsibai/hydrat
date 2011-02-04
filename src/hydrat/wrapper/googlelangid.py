f3f2d26d277a765ddef03093ec4dc284af797043
Marco Lui <saffsd@gmail.com>
2011-02-04 11:31:34 +1100

    changed to urllib2, added confidence output

--- src/hydrat/wrapper/googlelangid.py
+++ src/hydrat/wrapper/googlelangid.py
@@ -3,7 +3,7 @@
 Wrapper for google's langid API
 based on code from http://stackoverflow.com/questions/1136604/how-do-i-use-the-json-google-translate-api
 """
-import urllib
+import urllib, urllib2
 import json
 import time
 import random
@@ -11,13 +11,15 @@ import logging
 logger = logging.getLogger(__name__)
 
 class GoogleLangid(object):
-  base_url='http://ajax.googleapis.com/ajax/services/language/detect?'
+  base_url='http://ajax.googleapis.com/ajax/services/language/detect'
 
-  def __init__( self, sleep=5, simulate=False, retry = 60, apikey=None ):
+  def __init__( self, sleep=5, simulate=False, retry = 60, apikey=None, referer=None, confidence=False ):
     self.sleep = sleep # Time between requests
     self.simulate = simulate
     self.retry = retry
     self.apikey = apikey
+    self.referer = referer
+    self.confidence = confidence
 
   def classify(self, text):
     if isinstance(text, unicode): text = text.encode('utf-8')
@@ -28,7 +30,11 @@ class GoogleLangid(object):
       if self.apikey is not None:
         query['key'] = self.apikey
       data = urllib.urlencode(query)
-      search_results = urllib.urlopen(self.base_url+data)
+      req = urllib2.Request(self.base_url+'?'+data)
+      if self.referer is not None: req.add_header('Referer',self.referer)
+      search_results = urllib2.urlopen(req)
+
+      #search_results = urllib.urlopen(self.base_url+data)
       response = json.loads(search_results.read())
     retry = self.retry
     while response['responseData'] is None:
@@ -36,19 +42,21 @@ class GoogleLangid(object):
       logger.warning("Got a None response, retrying in %d seconds", retry)
       time.sleep(retry)
       retry *= 2
-      search_results = urllib.urlopen(self.base_url+data)
+      search_results = urllib2.urlopen(req)
       try:
         response = json.loads(search_results.read())
       except ValueError:
         response = {'responseData': None}
     result = response['responseData']['language']
-    return result
+    confidence = float(response['responseData']['confidence'])
+    if self.confidence:
+      return result, confidence
+    else:
+      return result
 
   def batch_classify(self, texts):
-    result = []
     for text in texts:
-      result.append(self.classify(text))
+      yield self.classify(text)
       time.sleep(random.random() * self.sleep)
-    return result
       
 
