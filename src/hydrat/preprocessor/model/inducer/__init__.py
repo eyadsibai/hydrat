1dedb73fb62d6d93f452462df47dc8b6c1c18121
Marco Lui <saffsd@gmail.com>
2011-02-11 10:53:03 +1100

    removed a bunch of unused functions

--- src/hydrat/preprocessor/model/inducer/__init__.py
+++ src/hydrat/preprocessor/model/inducer/__init__.py
@@ -1,26 +1,4 @@
 import numpy
-from collections import defaultdict
-
-def invert_text(text, tokenizer):
-  fv = defaultdict(int)
-  for token in tokenizer(text):
-    fv[token] += 1
-  return fv
-
-def invert_map(instance_map, tokenizer):
-  fvs = [ invert_text(instance_map[id], tokenizer) for id in instance_map ]
-  return fvs
-      
-def dense_repr(sparse_data, features):
-  matrix = numpy.zeros((len(sparse_data), len(features)),  dtype='int64')
-  feature_indices = dict( (k, v) for v, k in enumerate(features))
-  for i, fv in enumerate(sparse_data):
-    for f in fv:
-      j = feature_indices[f]
-      matrix[i,j] = fv[f]
-  return matrix
-
-import numpy
 def map2matrix(mapping, instance_ids=None, labels=None):
   #TODO: Sanity checks on instance_ids and labels
   if instance_ids is None:
@@ -46,10 +24,3 @@ def matrix2map(matrix, instance_ids, labels):
     id_labels = list(labels[row])
     mapping[id] = id_labels
   return mapping
-    
-
-# Has been renamed to map2matrix
-from hydrat.common.decorators import deprecated
-@deprecated
-def class_matrix(class_map, instance_ids, classlabels):
-  return map2matrix(class_map, instance_ids, classlabels)
