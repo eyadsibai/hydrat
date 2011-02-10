18a6e7247725cd744dbf22cd9256c8d7a6d75998
Marco Lui <saffsd@gmail.com>
2011-02-11 10:53:03 +1100

    removed a bunch of unused functions

--- src/hydrat/frameworks/online.py
+++ src/hydrat/frameworks/online.py
@@ -14,7 +14,6 @@ from hydrat.store import Store, StoreError, NoData, AlreadyHaveData
 from hydrat.preprocessor.model.inducer.dataset import DatasetInducer
 from hydrat.common import as_set
 from hydrat.preprocessor.features.transform import union
-from hydrat.preprocessor.model.inducer import invert_text
 from hydrat.common.pb import ProgressIter
 from hydrat.frameworks.common import Framework
 
