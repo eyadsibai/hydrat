713092f8dce5b8e4f17f24eb99de8edb405333b8
Marco Lui <saffsd@gmail.com>
2011-03-10 15:38:12 +1100

    added an encoding classmap to all encoded text datasets

--- src/hydrat/dataset/encoded.py
+++ src/hydrat/dataset/encoded.py
@@ -1,7 +1,26 @@
+import codecs
 from collections import defaultdict
 from hydrat.dataset.text import TextDataset
 from hydrat.common.pb import ProgressIter
 import hydrat.common.extractors as ext
+
+PYTHON_ENCODINGS = [
+'UNKNOWN', 'ascii', 'big5', 'big5hkscs', 'cp037', 'cp1006', 'cp1026', 
+'cp1140', 'cp1250', 'cp1251', 'cp1252', 'cp1253', 'cp1254', 'cp1255', 'cp1256', 
+'cp1257', 'cp1258', 'cp424', 'cp437', 'cp500', 'cp737', 'cp775', 'cp850', 
+'cp852', 'cp855', 'cp856', 'cp857', 'cp860', 'cp861', 'cp862', 'cp863', 
+'cp864', 'cp865', 'cp866', 'cp869', 'cp874', 'cp875', 'cp932', 'cp949', 
+'cp950', 'euc_jis_2004', 'euc_jisx0213', 'euc_jp', 'euc_kr', 'gb18030', 
+'gb2312', 'gbk', 'hz', 'iso2022_jp', 'iso2022_jp_1', 'iso2022_jp_2', 
+'iso2022_jp_2004', 'iso2022_jp_3', 'iso2022_jp_ext', 'iso2022_kr', 'iso8859-1', 
+'iso8859-10', 'iso8859-13', 'iso8859-14', 'iso8859-15', 'iso8859-16', 
+'iso8859-2', 'iso8859-3', 'iso8859-4', 'iso8859-5', 'iso8859-6', 'iso8859-7', 
+'iso8859-8', 'iso8859-9', 'johab', 'koi8-r', 'koi8-u', 'mac-cyrillic', 
+'mac-greek', 'mac-iceland', 'mac-latin2', 'mac-roman', 'mac-turkish', 
+'ptcp154', 'shift_jis', 'shift_jis_2004', 'shift_jisx0213', 'utf-16', 
+'utf-16-be', 'utf-16-le', 'utf-32', 'utf-32-be', 'utf-32-le', 'utf-7', 'utf-8', 
+'utf-8-sig',
+]
 
 class EncodedTextDataset(TextDataset):
   def __init__(self):
@@ -19,6 +38,19 @@ class EncodedTextDataset(TextDataset):
     if self.__encodings is None:
       self.__encodings = self.encodings()
     return self.__encodings
+
+  def cs_encoding(self):
+    return PYTHON_ENCODINGS
+
+  def cm_encoding(self):
+    encodings = self._encodings()
+    cm = {}
+    for instance_id in self.instance_ids:
+      try:
+        cm[instance_id] = [codecs.lookup(encodings[instance_id]).name]
+      except LookupError:
+        cm[instance_id] = ['UNKNOWN']
+    return cm
 
   def ts_codepoint(self):
     text = self.tokenstream('byte')
