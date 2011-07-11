aa07c59a1a715226e5b1c4e338eab5ef73b6415e
Marco Lui <saffsd@gmail.com>
2011-07-11 16:22:19 +1000

    implemented a parallelized genia tagging

--- src/hydrat/dataset/tokenstream.py
+++ src/hydrat/dataset/tokenstream.py
@@ -5,6 +5,7 @@ from hydrat.wrapper.genia import GeniaTagger
 from hydrat import config
 from hydrat.configuration import Configurable, EXE, DIR
 
+import multiprocessing as mp
 #TODO: Keep partial tokenstreams in shelve objects in scratch space 
 #      - this will avoid recomputation if it cuts out halfway somehow.
  
@@ -15,6 +16,14 @@ class PorterStem(EncodedTextDataset):
     streams = dict( (i,stemmer.process(text[i].encode('utf8'))) for i in ProgressIter(text,'Porter Stemmer') )
     return streams
 
+def process(text):
+  tagger_exe = config.getpath('tools','genia')
+  genia_path = config.getpath('tools','genia_data')
+  stemmer = GeniaTagger(tagger_exe, genia_path)
+  text = stemmer.process(text)
+  del stemmer
+  return text
+
 class Genia(Configurable, EncodedTextDataset):
   requires =\
     { ('tools','genia')      : EXE('geniatagger')
@@ -23,9 +32,13 @@ class Genia(Configurable, EncodedTextDataset):
 
   def ts_genia(self):
     text = self.ts_codepoint()
-    tagger_exe = config.getpath('tools','genia')
-    genia_path = config.getpath('tools','genia_data')
-    stemmer = GeniaTagger(tagger_exe, genia_path)
-    streams = dict( (i,stemmer.process(text[i].encode('utf8'))) for i in ProgressIter(text,'GENIA Tagger') )
+    keys = text.keys()
+    def text_iter():
+      for i in ProgressIter(keys,'GENIA Tagger'):
+        yield text[i].encode('utf8')
+
+    pool = mp.Pool(mp.cpu_count())
+    vals = pool.imap(process, text_iter())
+    streams = dict(zip(keys, vals))
     return streams
 
