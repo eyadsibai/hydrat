aaffd30c034369fef080364fd5924eaacfe628ef
Marco Lui <saffsd@gmail.com>
2012-03-07 17:53:41 +1100

    added ability to only retain part of the genia output

--- src/hydrat/dataset/tokenstream.py
+++ src/hydrat/dataset/tokenstream.py
@@ -6,6 +6,11 @@ from hydrat import config
 from hydrat.configuration import Configurable, EXE, DIR
 
 import multiprocessing as mp
+from itertools import izip
+import logging
+
+logger = logging.getLogger(__name__)
+
 #TODO: Keep partial tokenstreams in shelve objects in scratch space 
 #      - this will avoid recomputation if it cuts out halfway somehow.
  
@@ -16,13 +21,20 @@ class PorterStem(EncodedTextDataset):
     streams = dict( (i,stemmer.process(text[i].encode('utf8'))) for i in ProgressIter(text,'Porter Stemmer') )
     return streams
 
-def process(text):
-  tagger_exe = config.getpath('tools','genia')
-  genia_path = config.getpath('tools','genia_data')
-  stemmer = GeniaTagger(tagger_exe, genia_path)
-  text = stemmer.process(text)
-  del stemmer
-  return text
+def init_tagger(tagger_exe, genia_path):
+  global __tagger
+  __tagger = GeniaTagger(tagger_exe, genia_path)
+  logger.debug('initialized GeniaTagger instance')
+  
+def genia_full(text):
+  global __tagger
+  tokens = __tagger.process(text)
+  return tokens 
+
+def genia_pos(text):
+  global __tagger
+  tokens = __tagger.process(text, find_span=False)
+  return [ t.POStag for t in tokens ] 
 
 class Genia(Configurable, EncodedTextDataset):
   requires =\
@@ -30,15 +42,24 @@ class Genia(Configurable, EncodedTextDataset):
     , ('tools','genia_data') : DIR('geniatagger-3.0.1')
     }
 
-  def ts_genia(self):
+  def genia_tag(self, tag_fn):
     text = self.ts_codepoint()
     keys = text.keys()
     def text_iter():
       for i in ProgressIter(keys,'GENIA Tagger'):
         yield text[i].encode('utf8')
 
-    pool = mp.Pool(config.getint('parameters','cpu_count'))
-    vals = pool.imap(process, text_iter())
-    streams = dict(zip(keys, vals))
+    tagger_exe = config.getpath('tools','genia')
+    genia_path = config.getpath('tools','genia_data')
+    pool = mp.Pool(config.getint('parameters','job_count'), init_tagger, 
+        (tagger_exe, genia_path))
+    vals = pool.imap(tag_fn, text_iter())
+    streams = dict(izip(keys, vals))
     return streams
+
+  def ts_genia(self):
+    return self.genia_tag(genia_full)
+
+  def ts_geniapos(self):
+    return self.genia_tag(genia_pos)
 
