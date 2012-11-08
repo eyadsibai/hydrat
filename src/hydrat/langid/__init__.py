31463e6e5ce17c3775a7efafa4757b268d570c61
Marco Lui <saffsd@gmail.com>
2012-11-08 16:32:36 +1100

    added a buch of wrappers for external langid tools

--- src/hydrat/langid/__init__.py
+++ src/hydrat/langid/__init__.py
@@ -2,4 +2,7 @@ from textcat import TextCat
 from langdetect import LangDetect
 from google import GoogleLangid
 from bing import BingLangid
-
+from langiddotpy import LangidDotPy
+from chromecld import ChromeCLD
+from libtextcat import LibTextCat
+from lastrings import LAStrings
