0814e873ac3c9492865a8f5b9097a7534a72c5ba
Marco Lui <saffsd@gmail.com>
2012-12-14 17:21:33 +1100

    simplified metadata inclusion in summary

--- src/hydrat/browser/browser_config.py
+++ src/hydrat/browser/browser_config.py
@@ -5,17 +5,17 @@ from hydrat.result.interpreter import SingleHighestValue
 interpreter = SingleHighestValue()
 
 relevant  = [
-  ( {'label':"Dataset", 'searchable':True}       , "dataset"       ),
-  ( {'label':"Class Space",'searchable':True}     , "class_space"     ),
-  ( {'label':"Feature Desc",'searchable':True}   , "feature_desc"     ),
-  ( {'label':"Learner",'searchable':True}    , "learner"    ),
-  ( {'label':"Params",'searchable':True}    , "learner_params"    ),
+  ( {'label':"Dataset", 'searchable':True}       , "metadata:dataset"       ),
+  ( {'label':"Class Space",'searchable':True}     , "metadata:class_space"     ),
+  ( {'label':"Feature Desc",'searchable':True}   , "metadata:feature_desc"     ),
+  ( {'label':"Learner",'searchable':True}    , "metadata:learner"    ),
+  ( {'label':"Params",'searchable':True}    , "metadata:learner_params"    ),
   ( "Macro-F"       , "macro_fscore"        ),
   ( "Macro-P"     , "macro_precision"     ),
   ( "Macro-R"        , "macro_recall"        ),
   ( "Micro-F"       , "micro_fscore"        ),
   ( "Micro-P"     , "micro_precision"     ),
   ( "Micro-R"        , "micro_recall"        ),
-  ( {'sorter':'digit', 'label':"Learn Time"}    , "avg_learn"     ),
-  ( {'sorter':'digit', 'label':"Classify Time"} , "avg_classify"  ),
+  ( {'sorter':'digit', 'label':"Learn Time"}    , "metadata:avg_learn"     ),
+  ( {'sorter':'digit', 'label':"Classify Time"} , "metadata:avg_classify"  ),
 ]
