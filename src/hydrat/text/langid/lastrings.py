2bbf82b0ab3a92468646750bb3a47556a2762d4c
Marco Lui <saffsd@gmail.com>
2013-08-05 16:43:45 +1000

    added whatlang wrappeR

--- src/hydrat/text/langid/lastrings.py
+++ src/hydrat/text/langid/lastrings.py
@@ -1,44 +1,53 @@
 from hydrat.text import TextClassifier
 from hydrat import config
-from hydrat.configuration import Configurable, DIR, EXE
+from hydrat.configuration import Configurable, EXE, FILE
 
 import subprocess
 import tempfile
 import os
 import csv
 from contextlib import closing
-from collections import Counter
-class LAStrings(Configurable, TextClassifier):
+
+class WhatLang(Configurable, TextClassifier):
   """
-  Wrapper for la-strings
+  Wrapper for the whatlang identifier embedded in la-strings
   http://sourceforge.net/projects/la-strings/
   """
   requires={
-    ('tools','lastrings')        : EXE('la-strings'),
-    ('tools','lastrings-data')   : DIR('LAStrings-1.18'),
+    ('tools','whatlang')        : EXE('whatlang'),
+    ('tools','whatlang-db')        : FILE('languages.db'),
     }
 
   metadata = dict(
     class_space = 'iso639_1',
-    dataset='lastrings',
-    instance_space='lastrings',
-    learner='lastrings',
+    dataset='la-strings',
+    instance_space='whatlang',
+    learner='whatlang',
     learner_params={},
     )
 
   def __init__(self):
-    self.tooldir   = config.getpath('tools','lastrings-data')
-    self.toolpath  = config.getpath('tools','lastrings')
+    self.toolpath  = config.getpath('tools','whatlang')
+    self.tooldb    = config.getpath('tools','whatlang-db')
     self.tempdir   = config.getpath('paths','scratch')
     TextClassifier.__init__(self, lambda l: l if len(l) == 2 else 'UNKNOWN')
 
   def classify(self, text):
     with tempfile.NamedTemporaryFile(delete=False, dir=self.tempdir) as f:
       f.write(text)
-    p = subprocess.Popen([self.toolpath,'-i','-I','1',f.name], cwd=self.tooldir, stdout=subprocess.PIPE)
+    cmd = [self.toolpath,'-l'+self.tooldb,'-b0','-n1','-t',f.name]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    out = p.stdout.read()
+    lang = out.split(':')[0]
 
-    preds = Counter(row.split('\t',1)[0] for row in p.stdout)
     os.unlink(f.name)
-    retval =  [ l for l, freq in preds.most_common(1) ]
+    retval =  [ lang ]
+    return retval
+
+  def classify_batch(self, texts, callback=None):
+    cmd = [self.toolpath,'-l'+self.tooldb,'-b1','-n1','-t']
+    p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    stdout, stderr = p.communicate('\n'.join(l.replace('\n',' ') for l in texts))
+    retval = [ [row.split('\t',1)[0]] for row in stdout.splitlines() ]
     return retval
 
