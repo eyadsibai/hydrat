509b25e57c2f585062b43d57ec2c3475671ac85b
Marco Lui <saffsd@gmail.com>
2013-08-05 16:43:27 +1000

    made libtextcat config through hydratrc

--- src/hydrat/text/langid/libtextcat.py
+++ src/hydrat/text/langid/libtextcat.py
@@ -1,6 +1,15 @@
+from hydrat import config
 from hydrat.text import TextClassifier
+from hydrat.configuration import Configurable, DIR, FILE 
+
 import textcat
+
 class LibTextCat(TextClassifier):
+  requires={
+    ('tools','libtextcat-config') : FILE('conf.txt'),
+    ('tools','libtextcat-base')   : DIR('LM'),
+    }
+
   metadata = dict(
     class_space = 'iso639_1',
     dataset='libtextcat',
@@ -9,13 +18,13 @@ class LibTextCat(TextClassifier):
     learner_params={},
     )
 
-  def __init__(self, config, base=None):
+  def __init__(self):
     TextClassifier.__init__(self, None) # TODO output map
-    self.config = config
-    self.base = base
-    self.textcat = textcat.TextCat(config, base)
-    self.metadata['learner_params']['config'] = config
-    self.metadata['learner_params']['base'] = base
+    self.config = config.getpath('tools','libtextcat-config')
+    self.base = config.getpath('tools','libtextcat-base')
+    self.textcat = textcat.TextCat(self.config, self.base)
+    self.metadata['learner_params']['config'] = self.config
+    self.metadata['learner_params']['base'] = self.base
 
   def classify(self, text):
     try:
