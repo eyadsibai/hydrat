0fc2b4875b942f92a36461401e3fbba8931532b1
Marco Lui <saffsd@gmail.com>
2013-08-05 16:44:52 +1000

    fixed langdetect wrapper's issues with pytables lists of texts

--- src/hydrat/text/langid/langdetect.py
+++ src/hydrat/text/langid/langdetect.py
@@ -46,6 +46,8 @@ class LangDetect(Configurable, langdetect.LangDetect, TextClassifier):
     return langdetect.LangDetect.classify(self, text)
 
   def classify_batch(self, texts, callback=None):
-    return map(self.label_map, langdetect.LangDetect.classify_batch(self, texts, callback))
+    raw = langdetect.LangDetect.classify_batch(self, texts, callback)
+    retval = map(self.label_map, raw)
+    return retval
   
     
